857a8fa0981aae3e5ace87f215bb5183405b04bc
John Philip <developerphilo@gmail.com>
2021-10-17 11:49:24 -0700

    Implemented `clone <repo_name>` functionality command on the ClI

---
+++ src/clone.js
@@ -0,0 +1,22 @@
+import arg from 'arg';
+import chalk from 'chalk';
+import { stderr } from 'process';
+const {exec}=require('child_process');
+
+
+// 
+export async function Clone(arg) {
+    console.log(arg[2])
+    if (arg[2]) {
+        exec(`git clone ${arg[2]}`,(error,stdout,stderr)=>{
+            if (error) {
+                console.log(chalk.cyanBright(error));
+            }
+
+        })
+        
+    }else{
+        console.log(chalk.cyanBright('Please pass the repository as an argument to clone'));
+    }
+
+}
