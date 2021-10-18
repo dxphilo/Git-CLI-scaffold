8dedb582865d0173f8b1a910911245ccc0119ad7
John Philip <developerphilo@gmail.com>
2021-10-18 13:13:37 -0700

    updated figlet text info

--- bin/repo.js
+++ bin/repo.js
@@ -1,3 +1,12 @@
 #! /usr/bin/env node
 require = require('esm')(module /*, options*/);
 require('../src/clone').Clone(process.argv);
+
+const chalk = require('chalk');
+const figlet = require('figlet');
+
+if (process.argv){
+    console.log(chalk.magentaBright(
+        figlet.textSync('Clone Repo !!!', { horizontalLayout: 'full' })));
+}
+
